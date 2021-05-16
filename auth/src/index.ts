0e1c5e40baa38e822ddffb3d3808f33519da9712
Diep <n08ni.dieppn@gmail.com>
2021-05-16 21:09:11 +0700

    feat: async error handling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import 'express-async-errors';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { json } from 'body-parser';
 import { logger } from './logger';
@@ -18,7 +19,7 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
-app.all('*', () => {
+app.all('*', async () => {
   throw new NotFoundError();
 });
 
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -16,7 +16,7 @@ router.post('/api/users/signup',
       .isLength({ max: 20, min: 4 })
       .withMessage('Password must be between 4 and 20 characters')
   ], 
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
