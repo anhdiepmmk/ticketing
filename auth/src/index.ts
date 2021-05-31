8bbad5451d8a00a4e7c184317f857bf20d57e2e7
Diep <n08ni.dieppn@gmail.com>
2021-06-01 06:43:22 +0700

    mongoose connect

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,7 @@ import 'express-async-errors';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { json } from 'body-parser';
 import { logger } from './logger';
+import mongoose from 'mongoose';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -25,18 +26,38 @@ app.all('*', async () => {
 
 app.use(errorHandler);
 
-const PORT = 3000;
-const server = app.listen(PORT, () => {
-  logger.info(`Listing on port ${PORT}!`);
-});
+const start = async () => {
+  try {
+    logger.info('Mongoose connecting');
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    logger.info('Mongoose connected');
+  } catch (error) {
+    logger.error('Mongoose connecting', error);
+  }
 
-const gracefulShutDown = () => {
-  logger.info('Received SIGINT or SIGTERM.');
-  logger.info('Closing http server.');
-  server.close(() => {
-    logger.info('Http server closed');
+  const PORT = process.env.PORT || 3000;
+  const server = app.listen(PORT, () => {
+    logger.info(`Listing on port ${PORT}!`);
   });
+
+  const gracefulShutDown = () => {
+    logger.info('Received SIGINT or SIGTERM.');
+    logger.info('Closing http server.');
+    server.close(() => {
+      mongoose.disconnect(() => {
+        logger.info('Mongoose disconnected');
+      });
+      logger.info('Http server closed');
+    });
+  };
+
+  process.on('SIGTERM', gracefulShutDown);
+  process.on('SIGINT', gracefulShutDown);
 };
 
-process.on('SIGTERM', gracefulShutDown);
-process.on('SIGINT', gracefulShutDown);
+start();
+
